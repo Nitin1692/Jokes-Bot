f9c10796938b2049f6f0abdc43fec910bc8d3517
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2023-01-03 11:44:55 +0530

    Made a telegram bot for generating jokes

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+import pyjokes as py
+
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+bot =telebot.TeleBot(API_KEY)
+print(API_KEY)
+
+
+@bot.message_handler(commands=['Joke'])
+def print_joke(message):
+   a = py.get_joke()
+   bot.send_message(message.chat.id, a)
+
+
+@bot.message_handler(commands=['Start'])
+def start_command(message):
+    a= "Hello and welcome to Random joke Generator! Select one of the commands below to get started.(Please note, all the jokes are programming-related jokes, so don't be offended if you don't get some of them."
+    bot.send_message(message.chat.id, a)
+
+bot.polling()
